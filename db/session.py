aa6aedc2ace61f1445a60d90ea53b839d00fbac5
Moroz-Evgeny <ya.moroz2020@gmail.com>
2025-03-12 00:27:16 +0300

    bug

--- db/session.py
+++ db/session.py
@@ -1,20 +1,31 @@
 from typing import Generator
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 import settings
 
+##############################################
+# BLOCK FOR COMMON INTERACTION WITH DATABASE #
+##############################################
 
+# create async engine for interaction with database
+engine = create_async_engine(
+    settings.REAL_DATABASE_URL,
+    future=True,
+    echo=True,
+    execution_options={"isolation_level": "AUTOCOMMIT"},
+)
 
-engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
-
+# create session for the interaction with database
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 async def get_db() -> Generator:
-  try:
-    session: AsyncSession = async_session()
-    yield session
-  finally:
-    await session.close()
+    """Dependency for getting async session"""
+    try:
+        session: AsyncSession = async_session()
+        yield session
+    finally:
+        await session.close()
--- settings.py
+++ settings.py
@@ -1,13 +1,17 @@
+"""File with settings and configs for the project"""
 from envparse import Env
 
 env = Env()
 
 REAL_DATABASE_URL = env.str(
-  "REAL_DATABASE_URL", default="postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
-)
-TEST_DATABASE_URL = env.str(
-  "TEST_DATABASE_URL", default="postgresql+asyncpg://postgres_test:postgres_test@localhost:5433/postgres_test"
-)
+    "REAL_DATABASE_URL",
+    default="postgresql+asyncpg://postgres:postgres@localhost:5432/postgres",
+)  # connect string for the real database
+APP_PORT = env.int("APP_PORT", default=8000)
+
 SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
 ALGORITHM: str = env.str("ALGORITHM", default="HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
+# SENTRY_URL: str = env.str("SENTRY_URL")
+
+
